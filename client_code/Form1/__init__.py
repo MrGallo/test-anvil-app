12afefd490af448c2ad5fae5f4ec04ec2824d1be
dangallo110@gmail.com <dangallo110@gmail.com>
2023-12-13 02:47:33 +0000

    Created LICENSE.txt; Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,9 +1,13 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+import anvil.google.auth
 
 class Form1(Form1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-
+  
     # Any code you write here will run before the form opens.
+    email = anvil.google.auth.login()
